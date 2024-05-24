a7e77899fc51c780c75f49b4cf8a69d9f608548d
Daniela <danielaquinones.1203@gmail.com>
2024-05-24 13:35:19 -0500

    cambios

--- src/components/categories/CategoryIndex.vue
+++ src/components/categories/CategoryIndex.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container mt-4">
-      <h1 class="mb-4">Lista de Categorías</h1>
+      <h1 class="mb-4">Lista de las Categorías</h1>
       <router-link to="/categories/new" class="btn btn-success mb-3">Agregar Categoría</router-link>
       <table class="table table-striped table-hover">
         <thead class="thead-dark">
